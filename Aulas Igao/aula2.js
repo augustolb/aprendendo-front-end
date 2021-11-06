de30c3747904ad4ee913e7a3c5b53ef34508fa5f
augustolb <augusto.bombarda@gmail.com>
2021-11-06 16:58:21 -0300

    Inclusão das atividades realizar a partir das aulas

---
+++ Aulas Igao/aula2.js
@@ -0,0 +1,12 @@
+function funcao1 (number1, number2) {
+    const soma = number1 + number2
+    return soma;
+}
+
+const resultado = funcao1("igor", 6);
+
+console.log (resultado)
+console.log (resultado)
+console.log (resultado)
+console.log (resultado)
+
